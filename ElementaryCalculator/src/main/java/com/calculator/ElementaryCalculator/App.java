856640d4530bf282c9c634af032281db2a8b854f
Comfort <hluxxy7@gmail.com>
2023-08-27 12:01:05 +0200

    Initial Commit

---
+++ ElementaryCalculator/src/main/java/com/calculator/ElementaryCalculator/App.java
@@ -0,0 +1,13 @@
+package com.calculator.ElementaryCalculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Worldy!" );
+    }
+}
